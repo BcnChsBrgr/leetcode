cfc06f25328153977c05d340fe770f61ccc9802d
BcnChsBrgr <yuhinng41@gmail.com>
2023-10-01 15:32:59 +0100

    1492 completed

---
+++ 1492 The Kth Factor of n/solution.ts
@@ -0,0 +1,11 @@
+function kthFactor(n: number, k: number): number {
+    if (k === 1) return 1;
+    let index: number = 0;
+    for (let i = 0; i < n; i++) {
+        if (n % i == 0) {
+            index += 1;
+        }
+        if (index === k) return i;
+    }
+    return k - index > 1 ? -1 : n;
+}
