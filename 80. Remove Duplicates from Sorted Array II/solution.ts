3960e13c51f28bb3577760bfee0eff968e9bab2a
BcnChsBrgr <yuhinng41@gmail.com>
2024-05-13 23:07:39 +0100

    style(removeDuplicates): add some comments to explain the solution

--- 80. Remove Duplicates from Sorted Array II/solution.ts
+++ 80. Remove Duplicates from Sorted Array II/solution.ts
@@ -1,6 +1,9 @@
 function removeDuplicates(nums: number[]): number {
     let index: number = 2;
     for (let j = 2; j < nums.length; j++) {
+        // because it is a sorted array
+        // you only need to compare the current one with the prevous one
+        // [i] and [n-2]
         if (nums[j] > nums[index - 2]) {
             nums[index++] = nums[j];
         }
