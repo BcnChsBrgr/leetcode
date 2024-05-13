7e2abbc4f84e9ed7ac63ad50f00b4751e6cca341
BcnChsBrgr <yuhinng41@gmail.com>
2024-05-13 20:07:49 +0100

    style(majorityElement): use nullsafe for getting the number

--- 169. Majority Element/solution.ts
+++ 169. Majority Element/solution.ts
@@ -2,9 +2,7 @@ function majorityElement(nums: number[]): number {
     let shouldMoreThan: number = nums.length / 2;
     let trackMap = new Map<string, number>();
     for (let i: number = 0; i < nums.length; i++) {
-        let tmp: number = trackMap.has(`${nums[i]}`)
-            ? trackMap.get(`${nums[i]}`)
-            : 0;
+        let tmp: number = trackMap.get(`${nums[i]}`) ?? 0;
         trackMap.set(`${nums[i]}`, ++tmp);
 
         if (tmp > shouldMoreThan) {
